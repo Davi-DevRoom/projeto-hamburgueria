b94ff1ff0bb9b4fe9ff775f2459d4a8447ae71dd
Davi Luiz <127554027+Davi-DevRoom@users.noreply.github.com>
2025-03-03 09:53:25 -0400

    close/open + api whatsapp
    manipulação de aberto e fechado e envio de pedido para o whatsapp

--- script.js
+++ script.js
@@ -133,11 +133,51 @@ addressInput.addEventListener("input", function(event){
     }
 })
 
+// Finalizar pedido
 checkoutBtn.addEventListener("click", function(){
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        alert("RESTAURANTE FECHADO NO MOMENTO (ABRE AS 18h)");
+        return;
+    }
+
     if(cart.length === 0) return;
     if(addressInput.value === ""){
         addressWarn.classList.remove("hidden");
         addressInput.classList.add("border-red-500");
         return;
     }
+
+    //Enviar o pedido para a api Whatsapp
+    const cartItems = cart.map((item)=>{
+        return(
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price}`
+        )
+    }).join("");
+
+    const message = encodeURIComponent(cartItems);
+    const phone = "11994935432"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank");
+
+    cart = [];
+    updateCartModal();
 })
+
+//Verificar a hora e manipular o card horario
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("data-span");
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
